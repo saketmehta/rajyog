2064316e3b54a7d7f44119d588b73f936423e176
Saket Mehta <saket.m17@outlook.com>
2017-02-13 15:59:13 +0530

    initial commit
    angular 2 project for rajyog website

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { RajyogPage } from './app.po';
+
+describe('rajyog App', function() {
+  let page: RajyogPage;
+
+  beforeEach(() => {
+    page = new RajyogPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
